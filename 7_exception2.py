9dd2ca8b2993a6a1b918e42a01f83a62e23c32b6
Kirill Moguchii <moguchiikv@gmail.com>
2022-09-05 21:37:28 -0400

    7 fixed

--- 7_exception2.py
+++ 7_exception2.py
@@ -18,25 +18,19 @@ from typing import Type
 
 def discounted(price, discount, max_discount=20):
   try:
-    price = abs(price)
-    discount = abs(discount)
-    max_discount = abs(max_discount)
-  
-  except TypeError:
     price = abs(float(price))
     discount = abs(float(discount))
     max_discount = abs(int(max_discount))
-  
-  except TypeError:
-    print ("Input correct value") 
-
-  
-  if max_discount >= 100:
+      
+    if max_discount >= 100:
         print ('Слишком большая максимальная скидка')
-  elif discount >= max_discount:
+    elif discount >= max_discount:
         return price
-  else:
+    else:
         return price - (price * discount / 100)
+  
+  except (TypeError, ValueError):
+    print ("Input correct value") 
   
 
     
