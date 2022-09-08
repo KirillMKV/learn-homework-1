faed556d4afa6cefec395c637d9a11d680ce75a0
Kirill Moguchii <moguchiikv@gmail.com>
2022-09-07 23:56:40 -0400

    8 done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,6 +13,8 @@
 
 """
 import logging
+import ephem
+from datetime import datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -20,33 +22,38 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
+list_of_planets = [] # getting list of planets names from ephem
+for i in range(8):
+    list_of_planets.append(ephem._libastro.builtin_planets()[i][2].lower())
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
-def greet_user(update, context):
-    text = 'Вызван /start'
+def greet_user(update, context): # greetting message after /start command
+    text = f"Hello, user!"
     print(text)
     update.message.reply_text(text)
 
+def define_constellation(update, planet_name): # define constallation name for planet selected by user
+  planet = getattr(ephem, planet_name.capitalize())(ephem.now())
+  update.message.reply_text(str(ephem.constellation(planet)[1]))
 
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
 
+def talk_to_me(update, context): 
+    user_text = update.message.text.lower()
+    if user_text not in list_of_planets: # CHECK: is user's text name of the planet?
+      update.message.reply_text("Are you sure this is planet's name?")
+      update.message.reply_text("Please, input planet's name from Solar System")
+    else:
+      update.message.reply_text(define_constellation(update, user_text)) 
+    #update.message.reply_text(text)
+
+def get_planet_name(update, context):
+  update.message.reply_text('Input planet name')
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater("5457682533:AAETPwnjJ40znXNbMwKSIiezpOt2XvLvCIE", use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_planet_name))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
